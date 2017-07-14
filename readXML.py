145757c9dd08a4c1dc70b21308c31c95b9e21e37
smnra <hppall@163.com>
2017-07-14 09:38:20 +0800

    ET_iter 方式

--- readXML.py
+++ readXML.py
@@ -3,11 +3,10 @@
 import sys
 import xml.etree.cElementTree as ET
 if __name__=="__main__":
-   for event, elem in ET.iterparse("movies.xml", events=('end',)):#注意这里只使用end进行触发即可
+   for event, elem in ET.iterparse("all_lte_trace.xml", events=('end',)):#注意这里只使用end进行触发即可
          print(elem.tag, elem.text)
-         if elem.tag=='{raml20.xsd}managedObject':
-            print(elem.attrib)
-         elif elem.tag == '{raml20.xsd}p':
+
+         if elem.tag == '{raml20.xsd}ps':
             a_result = {}
             a_result=elem.attrib
             a_result['value']=elem.text
@@ -16,4 +15,6 @@ if __name__=="__main__":
             else:
                print(a_result)
 
+         elif elem.tag=='{raml20.xsd}managedObject':
+            print(elem.attrib)
          elem.clear()
