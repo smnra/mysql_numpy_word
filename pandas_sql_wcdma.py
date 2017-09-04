b7f935184f9bdd117761d97e0b86aa5145d4e2fb
smnra <hppall@163.com>
2017-09-04 11:34:47 +0800

    调整图表显示范围

--- pandas_sql_wcdma.py
+++ pandas_sql_wcdma.py
@@ -20,7 +20,7 @@ def getMonthFirstDay():
 
 tdate = getMonthFirstDay()
 #用sqlalchemy创建引擎
-sql = "select * from wcdma_day where 地市 <>'其他'  AND   日期>= '20170731' AND 日期 <  '20170901'"
+sql = "select * from city_wcdma_day where 地市 <>'其他'  AND   日期>= '20170801' AND 日期 <  '20170904'"
 engine = create_engine('mysql+pymysql://root:10300@192.168.3.74:50014/3g_kpi_browsing?charset=utf8')
 #df.to_sql('tick_data',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么  
 df1 = pd.read_sql(sql,engine)    #read_sql直接返回一个DataFrame对象      设置多个index，只要将index_col的值设置为列表
